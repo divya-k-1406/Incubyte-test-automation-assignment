7a61dcdde228c8e88636e0883071edaa51a44892
Divyadharshini k <divyadharshini.k1406@gmail.com>
2024-12-09 23:39:02 +0530

    feat: first working test case to check if the webpage is loaded

--- automation-assessment/src/test/java/com/incubyte/stepDefinitions/WebsiteVisit.java
+++ automation-assessment/src/test/java/com/incubyte/stepDefinitions/WebsiteVisit.java
@@ -5,8 +5,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class WebPageLaunchSteps extends BaseTest {
+public class WebsiteVisit extends BaseTest {
 
     @Given("the user is on the home page")
     public void userIsOnHomePage() {
@@ -14,12 +15,15 @@ public class WebPageLaunchSteps extends BaseTest {
         driver.get("https://magento.softwaretestingboard.com/");
     }
 
-    @When("the user identifies home page content")
+    @When("the user identifies contents from the home page")
     public void userIdentifiesHomePageContent() {
-        
+        String mainContentXpath = "//*[@id='maincontent']";
+        WebElement mainContent = driver.findElement(By.xpath(mainContentXpath));
+        System.out.println("tesxt" + mainContent.getText());
+        mainContent.click();
     }
 
-    @Then("the site launch is successful")
+    @Then("the user has loaded the page sucessfully")
     public void userLaunchesWebPageSuccessfully() {
         if (driver.getPageSource().contains("abc")) {
             System.out.println("Page is loaded successfully");
--- automation-assessment/src/test/java/com/incubyte/testRunners/testRunner.java
+++ automation-assessment/src/test/java/com/incubyte/testRunners/testRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
+    features = "src/resources/features",
     glue = "com.incubyte.stepDefinitions",
     plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
